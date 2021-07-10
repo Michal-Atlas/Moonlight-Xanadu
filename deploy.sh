bb9024f2214df95741767ce3e381e5df5fe175f8
Michal Atlas <michal.z.atlas@gmail.com>
2021-07-10 06:08:58 +0200

    Added Deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+git worktree add /tmp/gh-pages gh-pages
+emacs README.org --batch -f 'org-html-export-to-html' --kill
+mv README.html /tmp/gh-pages/index.html
+cd /tmp/gh-pages
+git add .
+git commit -m "$(date -Ins)"
+git push
+cd -
+git worktree remove /tmp/gh-pages
