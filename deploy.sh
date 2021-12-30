32f61b7a89d54c99398b81860821b67f8ae6802c
Michal Žáček <zacekmi2@fit.cvut.cz>
2021-12-30 01:54:52 +0100

    Tinkered with deploymeny features

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 set -euox pipefail
 
 git worktree add /tmp/gh-pages gh-pages
-printf "PDF version for download [[file:Moonlight-Xanadu.pdf][here]].\n\n%s" "$(cat Moonlight-Xanadu.org)" | pandoc -f org --toc -so index.html
-pandoc Moonlight-Xanadu.org --toc -so Moonlight-Xanadu.pdf
-mv index.html Moonlight-Xanadu.pdf /tmp/gh-pages/
-cd /tmp/gh-pages
+rm -r /tmp/gh-pages/*
+cp -r . /tmp/gh-pages
+cp Moonlight-Xanadu.pdf /tmp/gh-pages/
+cp Moonlight-Xanadu.html /tmp/gh-pages/index.html
+pushd /tmp/gh-pages
 git add .
 git commit -m "$(date -Ins)"
 git push
-cd -
+popd
 git worktree remove /tmp/gh-pages
